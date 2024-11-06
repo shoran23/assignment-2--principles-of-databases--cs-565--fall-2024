d3604ccedbbd914f094cd8dd8bb5a8b1694d6d9b
shoran23 <scottf.horan@gmail.com>
2024-11-05 21:35:20 -0500

    adjusted the indents on the sql files

--- database/create-devices-table.sql
+++ database/create-devices-table.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS devices (
-    serial_number   VARCHAR(12)   NOT NULL,
-    model_id        VARCHAR(24)   NOT NULL,
-    darwin          VARCHAR(8)    NOT NULL,
+  serial_number   VARCHAR(12)   NOT NULL,
+  model_id        VARCHAR(24)   NOT NULL,
+  darwin          VARCHAR(8)    NOT NULL,
 
-    FOREIGN KEY (model_id) REFERENCES models(model_id),
-    FOREIGN KEY (darwin) REFERENCES operating_systems(darwin),
+  FOREIGN KEY (model_id) REFERENCES models(model_id),
+  FOREIGN KEY (darwin) REFERENCES operating_systems(darwin),
 
-    PRIMARY KEY(serial_number)
+  PRIMARY KEY(serial_number)
 );
